eb8953b2b1faf4670e945f9bf71c54d7fd9bba84
Alex Waite <alex@waite.eu>
2017-08-22 11:30:19 +0200

    Add stub Features page

---
+++ content/pages/features.rst
@@ -0,0 +1,10 @@
+Features
+********
+:save_as: features.html
+:url: /features.html
+
+- `For Data Seekers </for/dataseekers.html>`_
+- `For Data Sharing </for/datasharing.html>`_
+- `For Git Users </for/gitusers.html>`_
+- `For Reproducible Science </for/reproducibility.html>`_
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -32,6 +32,7 @@ RELATIVE_URLS = False
 STATIC_PATHS = ['images', 'pics', 'css', 'js', 'img']
 MENUITEMS = [ ('About', '/about.html', None),
               ('Get DataLad', '/get_datalad.html', None),
+              ('Features', '/features.html', None),
               ('Datasets', '/datasets.html', None),
               ('Development', '/development.html', None),
               ('Docs', 'http://docs.datalad.org', '_blank'),
